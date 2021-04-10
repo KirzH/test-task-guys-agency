e496de9e813dcc9244ab672e60b51ee115e970b0
Vadim Kirzhakov <kirzhakov.2012@mail.ru>
2021-04-10 16:41:56 +0300

    fix editing main

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 let modal = document.getElementById("myModal")
 let btn = document.getElementById("btn_basket")
-let close = document.getElementById("close_modal_window")
+
 
 btn.onclick = function () {
 	modal.style.display = "block"
 }
 
-close.onclick = function () {
-	modal.style.display = "none"
-}
-
-window.onclick = function (event) {
-	if (event.target == modal) {
+window.onclick = function (e) {
+	if (e.target == modal) {
 		modal.style.display = "none"
 	}
 }
