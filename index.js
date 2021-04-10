36ae3d75e358ed8309bbdee1afa6baa72f55ebc3
Vadim Kirzhakov <kirzhakov.2012@mail.ru>
2021-04-10 19:29:56 +0300

    fix finished

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let modal = document.getElementById("myModal")
 let btn = document.getElementById("btn_basket")
 
-
 btn.onclick = function () {
 	modal.style.display = "block"
 }
+
+setTimeout(function () {
+	document.getElementById("myModal").style.display = "none"
+}, 3000)
 
 window.onclick = function (e) {
 	if (e.target == modal) {
